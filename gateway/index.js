3e4c2178136dde56fa334f8c1e2c23d04cfe4dc7
nahunicoR <nahunico.r@gmail.com>
2023-07-15 10:43:19 -0300

    gateway ready to go

---
+++ gateway/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const PORT = 8000;
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(
+    "/characters",
+    createProxyMiddleware({
+        target: "http://characters:8001",
+        changeOrigin: true,
+    })
+);
+
+app.use(
+    "/films",
+    createProxyMiddleware({
+        target: "http://films:8002",
+        changeOrigin: true,
+    })
+);
+
+app.use(
+    "/planets",
+    createProxyMiddleware({
+        target: "http://planets:8003",
+        changeOrigin: true,
+    })
+);
+
+app.use(
+    "/database",
+    createProxyMiddleware({
+        target: "http://planets:8004",
+        changeOrigin: true,
+    })
+);
+
+app.listen(PORT, () => console.log(`Gateway on port ${PORT}`));
